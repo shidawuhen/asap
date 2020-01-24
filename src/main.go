033dcca091a9484a6d5e176bd5032f1825692fca
pangzhiqiang <pangzhiqiang@xiaomi.com>
2020-01-24 23:30:06 +0800

    main

--- src/main.go
+++ src/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	"strings"
+	"time"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	/*r.ParseForm()
 	fmt.Println(r.Form)
 	fmt.Println("path: ", r.URL.Path)
 	fmt.Println("scheme: ", r.URL.Scheme)
@@ -16,13 +17,48 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Println("key: ", k)
 		fmt.Println("val: ", strings.Join(v, " "))
-	}
+	}*/
 	fmt.Fprintf(w, "hello chain!")
+
+}
+
+func computeBall() {
+	fmt.Println(33 * 33 * 33 * 33 * 33 * 33 * 16) //20663487504
+
+	redBall := []int{1, 4, 17, 31, 16, 5}
+	//blueBall := []int{9}
+	//01020304050601
+	//01020304050616
+
+	//33323130292816
+
+	redBallStart := 1
+	redBallEnd := 33
+	//blueBallStart := 1
+	//blueBallEnd := 16
+	rand.Seed(time.Now().UnixNano())
+
+	redBallLen := len(redBall)
+	//blueBallLen := len(blueBall)
+	sum := 0
+	same := false
+	for sum = 0; sum < 1000000000 && same == false; sum++ {
+		for i := 0; i < redBallLen; i++ {
+			redValue := redBallStart + rand.Intn(redBallEnd)
+			if redValue != redBall[i] {
+				break
+			}
+			if i == redBallLen-1 {
+				same = true
+			}
+		}
+	}
+	fmt.Println(sum)
 }
 
 func main() {
 	http.HandleFunc("/", sayHelloName)
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
