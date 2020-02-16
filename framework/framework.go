49dfb909b9afc2cb229b67bb043b8477a53babdd
pangzhiqiang <pangzhiqiang@xiaomi.com>
2020-02-16 11:33:55 +0800

    framework

---
+++ framework/framework.go
@@ -0,0 +1,94 @@
+package framework
+
+import (
+	"net/http"
+	"sync"
+
+	"fmt"
+)
+
+type HandlerFunc func(*Context)
+type HandlersChain []HandlerFunc
+
+//Context，用于处理请求的输入和输出数据
+type Context struct {
+	Request *http.Request
+	Writer  http.ResponseWriter
+}
+
+//将请求返回值写入http.Responsewriter中
+func (c *Context) String(format string, data ...interface{}) {
+	fmt.Fprintf(c.Writer, format, data...)
+	return
+}
+
+//核心结构，存放路由规则和使用pool获取与释放Context，减少GC
+type Engine struct {
+	pool   sync.Pool
+	router map[string]map[string]HandlersChain
+}
+
+//Engine初始化
+func New() *Engine {
+	fmt.Println("start")
+	engine := &Engine{}
+	engine.pool.New = func() interface{} {
+		return engine.allocateContext()
+	}
+	engine.router = make(map[string]map[string]HandlersChain, 0)
+	return engine
+}
+
+func (engine *Engine) allocateContext() *Context {
+	return &Context{}
+}
+
+//请求过来时http包会调用该函数
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := engine.pool.Get().(*Context)
+	//1.context初始化
+	c.Writer = w
+	c.Request = req
+	//2.真正的处理请求逻辑
+	engine.handleHTTPRequest(c)
+
+	engine.pool.Put(c)
+}
+
+func (engine *Engine) handleHTTPRequest(c *Context) {
+	httpMethod := c.Request.Method
+	rPath := c.Request.URL.Path
+	//从router中找到对应的方法并执行，如果不存在，则直接返回
+	routers, ok := engine.router[httpMethod]
+	if ok {
+		handles, ok := routers[rPath]
+		if ok {
+			for _, handle := range handles {
+				handle(c)
+			}
+			return
+		}
+	}
+	c.String("%s", httpMethod+" "+rPath+" doesnt exists")
+	return
+}
+
+//将路由添加到router中，没有并发操作，所以不加锁
+func (engine *Engine) AddRoute(method, path string, handlers ...HandlerFunc) {
+	//1.判断该http方法是否存在
+	_, ok := engine.router[method]
+	if !ok {
+		engine.router[method] = make(map[string]HandlersChain, 0)
+	}
+	//2.判断该路径是否存在，如果不存在则插入，如果存在，则不处理
+	_, ok = engine.router[method][path]
+	if !ok {
+		engine.router[method][path] = handlers
+	}
+}
+
+//运行服务，监听请求
+func (engine *Engine) Run(address string) (err error) {
+	err = http.ListenAndServe(address, engine)
+	return
+}
--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
-	"net/http"
-
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
-
 	_ "asap/docs"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	f "asap/framework"
 )
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", ping)
-
-	// 文档界面访问URL
-	// http://127.0.0.1:8080/swagger/index.html
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	return r
-}
-
-// @Summary 接口探活
-// @Produce  json
-// @Param lang query string false "en"
-// @Success 200 {string} string "ok"
-// @Router /ping [get]
-func ping(c *gin.Context) {
-	c.String(http.StatusOK, "ok")
+func ping(c *f.Context) {
+	fmt.Println(1)
+	c.String("%s", "ping")
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	e := f.New()
+	e.AddRoute("GET", "/ping", ping)
+	e.Run("127.0.0.1:9090")
 }
