ebce79d650b79278187a83af8fedc6b0d07953cb
pangzhiqiang <pangzhiqiang@xiaomi.com>
2020-09-02 12:15:30 +0800

    subarray

--- controller/algorithm/maximumsubarray.go
+++ controller/algorithm/maximumsubarray.go
@@ -1,7 +1,9 @@
 package algorithm
 
 /*
-原题：https://leetcode-cn.com/problems/maximum-subarray/
+原题：
+https://leetcode-cn.com/problems/maximum-subarray/
+https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/submissions/
 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 
 示例:
@@ -12,6 +14,7 @@ package algorithm
 
 分析：
 解法1：使用分治法，按文章所写方案计算
+解法2：从左往右遍历，将遍历的数据求和，如果和小于0，表示这个范围内的都没有用，因为和后面的相加，只会减小和值
 */
 
 func maxSum(nums []int, startIndex int, endIndex int) int {
@@ -53,6 +56,7 @@ func maxSum(nums []int, startIndex int, endIndex int) int {
 	return sum
 }
 
+//分治法
 //[-2,1,-3,4,-1,2,1,-5,4]
 func MaxSubArray(nums []int) int {
 	sum := 0
@@ -62,4 +66,27 @@ func MaxSubArray(nums []int) int {
 	}
 	sum = maxSum(nums, 0, length-1)
 	return sum
+}
+
+//简单解法
+//从左往右扫描，如果sum值小于0了，则放弃已经走过的值，重新开始
+//[-2,1,-3,4,-1,2,1,-5,4]
+func MaxSubArraySimple(nums []int) int {
+	sum := 0
+	maxSum := 0
+	length := len(nums)
+	if length == 0 {
+		return maxSum
+	}
+	maxSum = nums[0]
+	for i := 0; i < length; i++ {
+		sum += nums[i]
+		if sum > maxSum {
+			maxSum = sum
+		}
+		if sum < 0 {
+			sum = 0
+		}
+	}
+	return maxSum
 }
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	nums := []int{-2,1,-3,4,-1,2,1,-5,4}
-	algorithm.MaxSubArray(nums)
+	algorithm.MaxSubArraySimple(nums)
 	r := gin.Default()
 	InitRedis()
 	router.InitRouter(r)
