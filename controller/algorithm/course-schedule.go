87010896792feda715cec56daf355e5d7b4a1bda
pangzhiqiang <pangzhiqiang@xiaomi.com>
2020-10-23 23:40:10 +0800

    canfinish

--- controller/algorithm/course-schedule.go
+++ controller/algorithm/course-schedule.go
@@ -17,7 +17,7 @@ package algorithm
 输出: true
 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
 示例 2:
-
+          s  p
 输入: 2, [[1,0],[0,1]]
 输出: false
 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
@@ -28,41 +28,48 @@ package algorithm
 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
 你可以假定输入的先决条件中没有重复的边。
 1 <= numCourses <= 10^5
-*/
 
+
+分析：
+这道题的核心思路是，不断将没有父亲的节点删除，与该父亲连接的子节点的父亲会减一，如果没有回路，则所有节点都可以删除，
+否则一定无法删除所有节点
+
+这个想法还是很巧妙的
+*/
 
 func CanFinish(numCourses int, prerequisites [][]int) bool {
 	length := len(prerequisites)
 	if length == 0 {
 		return true
 	}
-	recordMap := make(map[int][]int)
+	parentNum := make([]int, numCourses)
+	parentSonList := make(map[int][]int)
 	for i := 0; i < length; i++ {
-		recordMap[prerequisites[i][0]] = append(recordMap[prerequisites[i][0]],prerequisites[i][1:len(prerequisites[i])]...)
+		parentNum[prerequisites[i][0]]++
+		parentSonList[prerequisites[i][1]] = append(parentSonList[prerequisites[i][1]], prerequisites[i][0])
 	}
-	//fmt.Println(recordMap)
-	for i := 0; i < length; i++ {
+	queue := make([]int, 0)
+	for i := 0; i < numCourses; i++ {
+		if parentNum[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
 
-		queue := make([]int,1)
-		queue[0] = prerequisites[i][0]
-		used := make([]bool,numCourses)
-		used[prerequisites[i][0]] = true
-		//fmt.Println(queue)
-		for j := 0;j < len(queue);j++  {
-			//fmt.Println(j,queue,used)
-			index := queue[j]
-			if list, ok:= recordMap[index];ok{
-				for m:=0;m < len(list);m++{
-					if used[list[m]] == true{
-						return false
-					}else{
-						used[list[m]] = true
-					}
+	for i := 0; i < len(queue); i++ {
+		numCourses--
+		index := queue[i]
+		if sonList, ok := parentSonList[index]; ok {
+			for j := 0; j < len(sonList); j++ {
+				parentNum[sonList[j]]--
+				if parentNum[sonList[j]] == 0 {
+					queue = append(queue, sonList[j])
 				}
-				queue = append(queue,list...)
 			}
 		}
-
 	}
-	return true
+	if numCourses == 0 {
+		return true
+	} else {
+		return false
+	}
 }
