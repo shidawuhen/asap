9fa50503c2a41a31c623099e8172df9590d68ca8
pangzhiqiang <pangzhiqiang@xiaomi.com>
2020-12-09 00:11:51 +0800

    reverse

---
+++ controller/algorithm/fan-zhuan-dan-ci-shun-xu-lcof.go
@@ -0,0 +1,49 @@
+package algorithm
+
+import "strings"
+
+/*
+原题：https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+剑指 Offer 58 - I. 翻转单词顺序
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+
+
+
+示例 1：
+
+输入: "the sky is blue"
+输出: "blue is sky the"
+示例 2：
+
+输入: "  hello world!  "
+输出: "world! hello"
+解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+示例 3：
+
+输入: "a good   example"
+输出: "example good a"
+解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
+
+说明：
+
+无空格字符构成一个单词。
+输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+*/
+
+func reverseWords(s string) string {
+	sa := strings.Split(s, " ")
+	res := ""
+	// fmt.Println(sa)
+	for i := len(sa) - 1; i >= 0; i-- {
+		if sa[i] != "" && sa[i] != " " {
+			res += sa[i]
+			if i != 0 {
+				res += " "
+			}
+		}
+	}
+	res = strings.Trim(res, " ")
+	return res
+}
