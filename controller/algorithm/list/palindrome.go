b1733c7df5a64853f9d273f11b521e4c349a67bc
pangzhiqiang <pangzhiqiang@bytedance.com>
2022-08-04 21:52:26 +0800

    twolist

--- controller/algorithm/list/palindrome.go
+++ controller/algorithm/list/palindrome.go
@@ -40,7 +40,7 @@ func isPalindrome(head *ListNode) bool {
 		fast = fast.Next
 		listLen++
 	}
-	//从slow到fast做反向
+	//从slow到fast做反向。不应该用这种方案，为啥要next、nn呢，要是用三个的话，当前的cur，然后找前一个和后一个，把自己放到中间最好处理
 	tmp := slow.Next
 	slow.Next = nil
 	var end *ListNode = nil
@@ -63,6 +63,38 @@ func isPalindrome(head *ListNode) bool {
 			return false
 		}
 		fast = fast.Next
+		h = h.Next
+	}
+	return true
+}
+
+func isPalindrome2(head *ListNode) bool {
+	if head == nil {
+		return true
+	}
+	//找到中间位置，这种方案需要slow往后走一步
+	slow := head
+	fast := head
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	//从slow往后逆序
+	cur := slow.Next
+	var pre *ListNode = nil
+	for cur != nil {
+		tmpNext := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = tmpNext
+	}
+	//从头到中间、从尾到中间
+	h := head
+	for pre != nil {
+		if pre.Val != h.Val {
+			return false
+		}
+		pre = pre.Next
 		h = h.Next
 	}
 	return true
